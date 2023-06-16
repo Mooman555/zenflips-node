fe2be327809bacd6e5332ca5242f447f1853fd56
Mooman555 <mooman@karigar.pk>
2023-06-16 18:04:15 +0500

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
 const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+// const cheerio = require('cheerio');
 const app = express();
 const PORT = process.env.PORT;
 const API_BASE_URL = process.env.API_BASE_URL;
